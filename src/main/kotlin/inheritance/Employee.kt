fd851d4323d9fe80002fb4e7e6c41588ca12b9cf
matiej <maciek.wojcik179@gmail.com>
2024-03-19 15:30:27 +0100

    refactor contstructor to smilyfie

--- src/main/kotlin/inheritance/Employee.kt
+++ src/main/kotlin/inheritance/Employee.kt
@@ -1,6 +1,11 @@
 package inheritance
 
-open class Employee(var name: String?, var surnmae: String?, var role: String?, var salary: Double?) {
+open class Employee(
+    var name: String?,
+    var surnmae: String?,
+    var role: String?,
+    var salary: Double?
+) {
 
     override fun toString(): String {
         return "I'm - (name=$name, surnmae=$surnmae, role=$role, salary=$salary)"
--- src/main/kotlin/inheritance/Programmer.kt
+++ src/main/kotlin/inheritance/Programmer.kt
@@ -1,6 +1,12 @@
 package inheritance
 
-class Programmer(name: String?, surname: String?, role: String?, salary: Double?, programmingLanguage: String) :
+class Programmer(
+    name: String?,
+    surname: String?,
+    role: String?,
+    salary: Double?,
+    programmingLanguage: String
+) :
     Employee(name, surname, role, salary) {
     var programmingLanguage: String? = programmingLanguage
 
