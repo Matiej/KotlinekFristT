45b3d50fb062b12f510a4579480e04af08e6cdf1
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-03-23 10:28:22 +0100

    anonymous class,  dataClass - IMPORTANT, objectClass

---
+++ src/main/kotlin/classeslearning/objectclass/MyApplicatonProperitesObject.kt
@@ -0,0 +1,7 @@
+package main.kotlin.classeslearning.objectclass
+
+object MyApplicatonProperitesObject {
+    const val myApplicationName: String = "MyApplication Amazing App"
+    var myApplicationVersion: String = "1.0.0"
+
+}
